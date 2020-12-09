5cedbd0cce1cac9d1453c325f0a8e3fe9c225de8
anita0711 <anita.ganani90@gmail.com>
2020-12-09 15:12:36 -0500

    Updates

---
+++ precache-manifest.0adbc0647054fce4e638706455b432b1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e13664494ccfdc303ae38c557b6ab40",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "10685c99db9c8147c315",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.348a5c97.chunk.css"
+  },
+  {
+    "revision": "2f1dd8ecb712423cae5b",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.ddc96845.chunk.js"
+  },
+  {
+    "revision": "10685c99db9c8147c315",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.273741a0.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "8859962407820ed1deceab325bb6546c",
+    "url": "/seainfo6150-final-project-webapp/static/media/AllPlaces.88599624.jpg"
+  },
+  {
+    "revision": "8368147e95322d5a76effcac412ffce7",
+    "url": "/seainfo6150-final-project-webapp/static/media/Disneyland.8368147e.jpg"
+  },
+  {
+    "revision": "b873beda74a41df377484d7de34fe43d",
+    "url": "/seainfo6150-final-project-webapp/static/media/Fall.b873beda.jpg"
+  },
+  {
+    "revision": "f22524509f72cac7dc2c08563ba653da",
+    "url": "/seainfo6150-final-project-webapp/static/media/Hiking.f2252450.jpg"
+  },
+  {
+    "revision": "83e5bbf6a20ad7949bba9bb0e4d6a9ea",
+    "url": "/seainfo6150-final-project-webapp/static/media/Home.83e5bbf6.jpg"
+  },
+  {
+    "revision": "bb3e29e98071a31ae6f50fe5751a4c19",
+    "url": "/seainfo6150-final-project-webapp/static/media/Kids.bb3e29e9.jpg"
+  },
+  {
+    "revision": "3c9a293691864c90b1b5b9bb79377020",
+    "url": "/seainfo6150-final-project-webapp/static/media/Lake-Tahoe.3c9a2936.jpg"
+  },
+  {
+    "revision": "24f3f23ecf73f6e8a0f6200f1ebec273",
+    "url": "/seainfo6150-final-project-webapp/static/media/NewYork.24f3f23e.jpg"
+  },
+  {
+    "revision": "db892ba855192943b0fb9b6d6f2bf6cd",
+    "url": "/seainfo6150-final-project-webapp/static/media/PageNotFound.db892ba8.jpg"
+  },
+  {
+    "revision": "1579f16e9289071c69338cbb4c6ab559",
+    "url": "/seainfo6150-final-project-webapp/static/media/Place-Details.1579f16e.jpg"
+  },
+  {
+    "revision": "13a6ea96efd9bff9f8c33aa38201be09",
+    "url": "/seainfo6150-final-project-webapp/static/media/Sightseeing.13a6ea96.jpg"
+  },
+  {
+    "revision": "aee534afa5fef72526a240dc5aa183c5",
+    "url": "/seainfo6150-final-project-webapp/static/media/Spring.aee534af.jpg"
+  },
+  {
+    "revision": "886b7f85135689ba73b44ed50fbf83a3",
+    "url": "/seainfo6150-final-project-webapp/static/media/Summer.886b7f85.jpg"
+  },
+  {
+    "revision": "56a243e040a7576a8ce0a2ca7d33d2d0",
+    "url": "/seainfo6150-final-project-webapp/static/media/Washington-DC.56a243e0.jpg"
+  },
+  {
+    "revision": "5587a2a2753ca9b6746a10d288fc30ef",
+    "url": "/seainfo6150-final-project-webapp/static/media/Winter.5587a2a2.jpg"
+  },
+  {
+    "revision": "036aa1e4d65a5e61fe5397a09c685178",
+    "url": "/seainfo6150-final-project-webapp/static/media/Yellowstone.036aa1e4.jpg"
+  },
+  {
+    "revision": "fb4774a1f6053da1276e4c8f8c2c43ab",
+    "url": "/seainfo6150-final-project-webapp/static/media/Yosemite.fb4774a1.jpg"
+  },
+  {
+    "revision": "f02008022aeaaf368175484c5c95b22e",
+    "url": "/seainfo6150-final-project-webapp/static/media/about-us.f0200802.jpg"
+  },
+  {
+    "revision": "4f9b14fdc25fb9955186627e6bf3b1e1",
+    "url": "/seainfo6150-final-project-webapp/static/media/feedback.4f9b14fd.png"
+  },
+  {
+    "revision": "648a42ff07392712adffc28024c44dcc",
+    "url": "/seainfo6150-final-project-webapp/static/media/loading.648a42ff.gif"
+  },
+  {
+    "revision": "0c59cadeb0f8ce094999fd1dddac6444",
+    "url": "/seainfo6150-final-project-webapp/static/media/logo.0c59cade.png"
+  },
+  {
+    "revision": "3a220484d41fcf8be0670d6f4e6da346",
+    "url": "/seainfo6150-final-project-webapp/static/media/thankyou.3a220484.jpg"
+  }
+]);
